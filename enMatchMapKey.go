83afdfe3534990194a6554fbc7a3d62fba1e0c79
drkisler <drkisler@gmail.com>
2023-11-25 09:02:00 +0800

    modify go.mod set module to github.com/drkisler/utils

--- enMatchMapKey.go
+++ enMatchMapKey.go
@@ -1,11 +1,58 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type TMatchMapKey struct {
 	Data map[string]string
 }
 
+func (ms *TMatchMapKey) GetInt(source map[string]interface{}, key string) (int, error) {
+	result, ok := source[ms.Data[key]]
+	if !ok {
+		return 0, fmt.Errorf("%s不存在", key)
+	}
+	if result == nil {
+		return 0, nil
+	}
+	v, ok := result.(int)
+	if !ok {
+		return 0, fmt.Errorf("%s不是整数类型", key)
+	}
+	return v, nil
+}
+
+func (ms *TMatchMapKey) GetInt8(source map[string]interface{}, key string) (int8, error) {
+	result, ok := source[ms.Data[key]]
+	if !ok {
+		return 0, fmt.Errorf("%s不存在", key)
+	}
+	if result == nil {
+		return 0, nil
+	}
+	v, ok := result.(int8)
+	if !ok {
+		return 0, fmt.Errorf("%s不是整数类型", key)
+	}
+	return v, nil
+}
+
+func (ms *TMatchMapKey) GetInt16(source map[string]interface{}, key string) (int16, error) {
+	result, ok := source[ms.Data[key]]
+	if !ok {
+		return 0, fmt.Errorf("%s不存在", key)
+	}
+	if result == nil {
+		return 0, nil
+	}
+	v, ok := result.(int16)
+	if !ok {
+		return 0, fmt.Errorf("%s不是整数类型", key)
+	}
+	return v, nil
+}
 func (ms *TMatchMapKey) GetInt32(source map[string]interface{}, key string) (int32, error) {
 	result, ok := source[ms.Data[key]]
 	if !ok {
@@ -69,6 +116,23 @@ func (ms *TMatchMapKey) GetString(source map[string]interface{}, key string) (st
 	}
 	return v, nil
 }
+func (ms *TMatchMapKey) GetFloat32(source map[string]interface{}, key string) (float32, error) {
+	result, ok := source[ms.Data[key]]
+	if !ok {
+		return 0.0, fmt.Errorf("%s不存在", key)
+	}
+	if result == nil {
+		return 0.0, nil
+	}
+	switch v := result.(type) {
+	case float32:
+		return v, nil
+	case float64:
+		return float32(v), nil
+	default:
+		return 0.0, fmt.Errorf("%s不是浮点数类型", key)
+	}
+}
 func (ms *TMatchMapKey) GetFloat64(source map[string]interface{}, key string) (float64, error) {
 	result, ok := source[ms.Data[key]]
 	if !ok {
@@ -87,4 +151,64 @@ func (ms *TMatchMapKey) GetFloat64(source map[string]interface{}, key string) (f
 	default:
 		return 0.0, fmt.Errorf("%s不是浮点数类型", key)
 	}
+}
+
+func (ms *TMatchMapKey) GetBool(source map[string]interface{}, key string) (bool, error) {
+	result, ok := source[ms.Data[key]]
+	if !ok {
+		return false, fmt.Errorf("%s不存在", key)
+	}
+	if result == nil {
+		return false, nil
+	}
+	v, ok := result.(bool)
+	if !ok {
+		return false, fmt.Errorf("%s不是布尔类型", key)
+	}
+	return v, nil
+}
+
+func (ms *TMatchMapKey) GetTime(source map[string]interface{}, key string) (time.Time, error) {
+	result, ok := source[ms.Data[key]]
+	if !ok {
+		return time.Time{}, fmt.Errorf("%s不存在", key)
+	}
+	if result == nil {
+		return time.Time{}, nil
+	}
+	v, ok := result.(time.Time)
+	if !ok {
+		return time.Time{}, fmt.Errorf("%s不是时间类型", key)
+	}
+	return v, nil
+}
+
+func (ms *TMatchMapKey) GetSlice(source map[string]interface{}, key string) ([]interface{}, error) {
+	result, ok := source[ms.Data[key]]
+	if !ok {
+		return nil, fmt.Errorf("%s不存在", key)
+	}
+	if result == nil {
+		return nil, nil
+	}
+	v, ok := result.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("%s不是切片类型", key)
+	}
+	return v, nil
+}
+
+func (ms *TMatchMapKey) GetStruct(source map[string]interface{}, key string) (interface{}, error) {
+	result, ok := source[ms.Data[key]]
+	if !ok {
+		return nil, fmt.Errorf("%s不存在", key)
+	}
+	if result == nil {
+		return nil, nil
+	}
+	_, ok = result.(struct{})
+	if !ok {
+		return nil, fmt.Errorf("%s不是结构体类型", key)
+	}
+	return result, nil
 }
